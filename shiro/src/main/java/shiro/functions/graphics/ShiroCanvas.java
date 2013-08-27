d1ae4b32570f2974dba60c7fccf13df29c3fda96
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2013-08-27 15:03:21 -0700

    Added a canvas component to handle rendering

--- shiro/src/main/java/shiro/functions/graphics/ShiroCanvas.java
+++ shiro/src/main/java/shiro/functions/graphics/ShiroCanvas.java
@@ -9,7 +9,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
-import java.awt.geom.Line2D;
 import java.util.List;
 import javax.swing.JComponent;
 
